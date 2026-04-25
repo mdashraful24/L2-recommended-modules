4f47805af32eeadfeadc87cacc09752659b53347
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-04-25 21:19:39 +0600

    added problem and solution

---
+++ module-04/RM04V03.js
@@ -0,0 +1,57 @@
+// Problem Statement
+
+// Given a string s containing just the characters "(", ")", "{", "}", "[" and "]",
+// determine if the input string is valid.
+// An input string is valid if:
+//     Open brackets must be closed by the same type of brackets.
+//     Open brackets must be closed in the correct order.
+//     Every close bracket has a corresponding open bracket of the same type.
+
+//? Input and Output
+//? "()[]{}" -> true
+//? "([{}])" -> true
+//? "(]" -> false
+//? "(()" -> false
+
+// Solution
+
+import Stack from "./stack/Stack.js";
+
+const bracketChecker = (str) => {
+    const stack = new Stack();
+
+    const bracketMap = {
+        ")": "(",
+        "}": "{",
+        "]": "[",
+    };
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+
+        if (char === "(" || char === "{" || char === "[") {
+            stack.push(char);
+        } else if (char === ")" || char === "}" || char === "]") {
+            if (stack.isEmpty()) {
+                return false;
+            }
+
+            const expected = bracketMap[char];
+            const got = stack.pop();
+
+            console.log("Expected: ", expected, "Got: ", got);
+
+            if (got !== expected) {
+                return false;
+            }
+        }
+    }
+
+    return stack.isEmpty();
+};
+
+console.log(bracketChecker("([{}])"));
+console.log("-----------------------");
+console.log(bracketChecker(")([{}])"));
+console.log("-----------------------");
+console.log(bracketChecker("([{})"));
